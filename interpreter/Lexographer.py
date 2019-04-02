96303c8cf2b22609e904763c5c977b17fe6313e1
Jared Scarito <TheWolfBadger@gmail.com>
2019-04-02 09:14:42 -0400

    Initial commit

---
+++ interpreter/Lexographer.py
@@ -0,0 +1,15 @@
+tokens = {
+    'OPEN_CURLY' : '{',
+    'CLOSE_CURLY': '}',
+    'OPEN_BRACE': '[',
+    'CLOSE_BRACE': ']',
+    'COMMA': ',',
+    'OPEN_PAREN': '(',
+    'CLOSE_PAREN': ')',
+    'SLASH': '/',
+    'BACKSLASH': '\\',
+    'NEW_LINE': ';',
+    '': '',
+    '': '',
+    '': '',
+}
