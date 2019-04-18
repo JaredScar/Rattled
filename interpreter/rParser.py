5b11ee3e023c9c09731b534fe6bd3ff68f83d25e
TheWolfBadger <TheWolfBadger@gmail.com>
2019-04-18 14:16:53 -0400

    - rParser parse_variable() function pretty much done

--- interpreter/rParser.py
+++ interpreter/rParser.py
@@ -3,7 +3,6 @@ class Parser(object):
         # Tokens created by the Lexer
         self.tokens = tokens
         self.variables = list()
-        self.varTypes = list()
     def parse(self):
         index = 0
         for token in self.tokens:
@@ -17,7 +16,10 @@ class Parser(object):
             if 'IDENTIFIER' in token_type:
                 if(len(self.tokens) > index + 1):
                     if self.tokens[index + 1][2] in '=':
-                        self.parse_variable(index)
+                        print() # TODO Get rid of
+                        varDec = self.parse_variable(index)
+                        print(varDec) # TODO Get rid of
+                        self.variables.append(varDec)
             if 'FUNCTION' in token_type:
                 # TODO Parse function
                 print()
@@ -32,9 +34,37 @@ class Parser(object):
     def parse_variable(self, startInd):
         # This is to set up a variable defined
         curInd = startInd
+        varName = ''
+        varVal = list()
         while curInd < len(self.tokens):
             lineNo = self.tokens[curInd][0]
             token_type = self.tokens[curInd][1]
             token_val = self.tokens[curInd][2]
-            # TODO
+            if curInd == startInd:
+                varName = token_val
+            elif curInd == startInd + 2:
+                # This is the first value of the variable
+                if token_type in 'IDENTIFIER' or token_type in 'STRING' or token_type in 'NUMBER':
+                    varVal.append([lineNo, token_type, token_val])
+                else:
+                    # ERROR, Invalid variable
+            # This is next value of variable if it is an operator before it
+            elif token_type in 'OPERATOR':
+                if token_val not in '=':
+                    varVal.append([lineNo, token_type, token_val])
+                    nextVal = self.tokens[curInd + 1]
+                    nextLine = nextVal[0]
+                    nextType = nextVal[1]
+                    nextVal = nextVal[2]
+                    if nextType in 'IDENTIFIER' or nextType in 'STRING' or nextType in 'NUMBER':
+                        varVal.append([nextLine, nextType, nextVal])
+                    else:
+                        # ERROR, Invalid variable
+                    curInd += 1
+            else:
+                break
             curInd += 1
+        return [varName, varVal]
+    def parse_function(self, startInd):
+        # This is to set up a parsed function for Rattled
+        # TODO
