e4bac1ed45b0961fb93a7b7aca312c4c36f06d26
TheWolfBadger <thewolfbadger@gmail.com>
2020-09-28 12:04:03 -0400

    + Rewriting lexer.py and rParser.py with actual commenting, so I can make this a real programming language finally :)

--- interpreter/lexer.py
+++ interpreter/lexer.py
@@ -6,12 +6,6 @@ import constants
 # Rattled Programming Language Lexer
 #
 # Lexing regex:
-'''
-variable declaration: 
-pr function:
-if statement: (if\(.*\)|if.\(.*\))                  Doesn't factor in that each open parenthesis has a closed one yet
-if body: {([^}]+)}                                  Encounters problem when another } in String
-'''
 class Lexer(object):
     def __init__(self, source_code):
         self.source_code = source_code
@@ -21,75 +15,64 @@ class Lexer(object):
 
         sourceTokenized = self.source_code
 
-        stringTracked = False
-        trackedString = ""
-        trackedSymbol = ""
-        for lineNo in range(len(sourceTokenized)):
-            for token in str(sourceTokenized[lineNo]).split():
-                wasCast = False
-                for cast in constants.casts:
-                    if (token[0:len(cast)] in cast):
-                        tokens.append([lineNo, 'CAST', token])
-                        wasCast = True
-                        break
-                # This tokenizes the string part of source code
-                if ('"' in token or "'" in token or stringTracked == True):
-                    # Make sure the string doesn't end on the first token:
-                    if not (token[0] in token[1:len(token)] and (token[0] in "'" or token[0] in '"')):
-                        # We are currently interpreting a string, so we keep adding it to the trackedString
-                        if (stringTracked == True and trackedSymbol in token):
-                            # This is where the string ends, since we already have been tracked a string
-                            trackedSymbol = ""
-                            stringTracked = False
-                            if (";" in token[len(token) - 1]):
-                                trackedString += token[0:len(token) - 2]
-                            else:
-                                trackedString += token[0:len(token) - 1]
-                            trackedString += ""
-                            tokens.append([lineNo, 'STRING', trackedString])
-                            trackedString = ""
+        currentString = ""
+        trackingString = False;
+        stringPrefix = None;
+        stringLineNo = None;
+        trackingIdentifier = False;
+        currentIdentifier = ""
+        commentSkip = False;
+
+        lineNo = 1;
+
+        for line in sourceTokenized:
+            # line represents each different line in the source code
+            print("The line is: " + line);
+            for v in line:
+                # v represents each single character in the line
+                if not commentSkip:
+                    print("The value is: '" + v + "'")
+                    if "'" in v or '"' in v:
+                        # The start of a string
+                        if trackingString and stringPrefix == v:
+                            # A string is being tracked, close the string now
+                            trackingString = False;
+                            tokens.append([stringLineNo, currentString, 'STRING']);
+                            currentString = "";
+                            stringPrefix = None;
+                            stringLineNo = None;
+                        elif not trackingString:
+                            # A string is now being tracked
+                            trackingString = True;
+                            stringPrefix = v;
+                            stringLineNo = lineNo;
+                    elif (re.match('[a-z]', v) or re.match('[A-Z]', v)):
+                        if trackingString:
+                            # It's a string, we add it to currentString
+                            currentString += v;
                         else:
-                            # This is still part of a string, add it to trackedString
-                            if (stringTracked == False):
-                                trackedString += token[1:len(token)] + " "
-                            else:
-                                trackedString += token + " "
-                            if (len(trackedSymbol) == 0 and '"' in token):
-                                trackedSymbol = '"'
-                            elif (len(trackedSymbol) == 0 and "'" in token):
-                                trackedSymbol = "'"
-                            stringTracked = True
-                    else:
-                        if ";" in token[len(token) - 1]:
-                            tokens.append([lineNo, 'STRING', token[1:len(token) - 2]])
-                        else:
-                            tokens.append([lineNo, 'STRING', token[1:len(token) - 1]])
-                # Is performing a function
-                elif (token in constants.functions):
-                    tokens.append([lineNo, 'FUNCTION', token])
-                # Is calling a Rattled keyword
-                elif (token in constants.keywords):
-                    tokens.append([lineNo, 'KEYWORD', token])
-                # The token was casted
-                elif (wasCast):
-                    tokens.append([lineNo, 'CAST', token])
-                    continue
-                # Is creating a variable identifier
-                elif (re.match('[a-z]', token) or re.match('[A-Z]', token)):
-                    if (";" in token):
-                        tokens.append([lineNo, 'IDENTIFIER', token[0:len(token) - 2]])
-                    else:
-                        tokens.append([lineNo, 'IDENTIFIER', token])
-                # Is creating an Integer
-                elif (re.match('[0-9]', token)):
-                    if (";" in token):
-                        tokens.append([lineNo, 'NUMBER', token[0:len(token) - 1]])
-                    else:
-                        tokens.append([lineNo, 'NUMBER', token])
-                # Is trying to perform an operation
-                elif (token in "- + = / * % ! += -= *= /= > < >= <="):
-                    tokens.append([lineNo, 'OPERATOR', token])
-        print(str(tokens)) # TODO Get rid of debugger
-        self.tokens = tokens
+                            # It's not a string, should be part of an identifier
+                            trackingIdentifier = True;
+                            currentIdentifier += v;
+                    elif (" " in v) or ('\n' in v):
+                        # It's a space, if there is a currentIdentifier or currentString, submit them
+                        if trackingString:
+                            # A string was being tracked, add the space in there
+                            currentString += v;
+                        elif trackingIdentifier:
+                            # A identifier was being tracked, now there is a space, submit it
+                            tokens.append([lineNo, currentIdentifier, 'IDENTIFIER']);
+                            currentIdentifier = "";
+                            trackingIdentifier = False;
+                    elif "#" in v:
+                        # The start of a comment, skip line
+                        commentSkip = True;
+                    elif v in "+ - = / *":
+                        # This is an operator
+                        tokens.append([lineNo, v, 'OPERATOR']);
+            lineNo += 1;
+            commentSkip = False;
+        print("Lexer Tokens: " + str(tokens))
+        self.tokens = tokens;
     def getTokens(self):
         return self.tokens
--- interpreter/rParser.py
+++ interpreter/rParser.py
@@ -1,4 +1,3 @@
-from Objects.VarObj import Variable
 
 class Parser(object):
     def __init__(self, tokens):
@@ -6,74 +5,6 @@ class Parser(object):
         self.tokens = tokens
         self.transpiled_code = ""
     def parse(self):
-        index = 0
-        for token in self.tokens:
-            lineNo = token[0]
-            token_type = token[1]
-            token_val = token[2]
-
-            # Parsing starts below:
-
-            # This is an IDENTIFIER being set to some value if it has an = after it
-            if 'IDENTIFIER' in token_type:
-                if(len(self.tokens) > index + 1):
-                    if self.tokens[index + 1][2] in '=':
-                        print() # TODO Get rid of
-                        varDec = self.parse_variable(index)
-                        print(varDec) # TODO Get rid of
-                        varTrans = Variable()
-                        self.transpiled_code += varTrans.transpile(varDec[0], varDec[1][2], varDec)
-            if 'FUNCTION' in token_type:
-                # TODO Parse function
-                print()
-            if 'KEYWORD' in token_type:
-                # TODO
-                print()
-            if 'CAST' in token_type:
-                # TODO
-                print()
-            index += 1
+        # TODO REWRITE
         print("Python code to execute: ")
         print(self.transpiled_code)
-    def parse_variable(self, startInd):
-        # This is to set up a variable defined
-        curInd = startInd
-        varName = ''
-        varVal = list()
-        while curInd < len(self.tokens):
-            lineNo = self.tokens[curInd][0]
-            token_type = self.tokens[curInd][1]
-            token_val = self.tokens[curInd][2]
-            if curInd == startInd:
-                varName = token_val
-            elif curInd == startInd + 2:
-                # This is the first value of the variable
-                if token_type in 'IDENTIFIER' or token_type in 'STRING' or token_type in 'NUMBER' or token_type in 'CAST':
-                    varVal.append([lineNo, token_type, token_val])
-                else:
-                    # ERROR, Invalid variable
-                    print("ERROR: Line No. " + str(lineNo) + " : Unexpected value: '" + str(token_val) + "'")
-                    quit()
-            # This is next value of variable if it is an operator before it
-            elif token_type in 'OPERATOR':
-                if token_val not in '=':
-                    varVal.append([lineNo, token_type, token_val])
-                    nextVal = self.tokens[curInd + 1]
-                    nextLine = nextVal[0]
-                    nextType = nextVal[1]
-                    nextVal = nextVal[2]
-                    if nextType in 'IDENTIFIER' or nextType in 'STRING' or nextType in 'NUMBER' or nextType in 'CAST':
-                        varVal.append([nextLine, nextType, nextVal])
-                    else:
-                        # ERROR, Invalid variable
-                        print("ERROR: Line No. " + str(nextLine) + " : Unexpected value: '" + str(nextVal) + "'")
-                        quit()
-                    curInd += 1
-            else:
-                break
-            curInd += 1
-        return [varName, self.tokens[startInd + 1], varVal]
-    def parse_function(self, startInd):
-        # This is to set up a parsed function for Rattled
-        print()
-        # TODO
